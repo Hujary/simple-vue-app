17e3023f87883883c69ae6bc42135f3099287438
Hujary <petzold.tom2000@gmail.com>
2023-03-23 15:33:56 +0100

    recentVersion 1.2

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
     <div> 
-      <h1> Barcode App</h1>
+      <h1 style="text-align: center;"> Barcode App</h1>
       <StreamBarcodeReader @decode="onDecode" @loaded="onLoaded" id="readerID"></StreamBarcodeReader>
     </div>
-
     <div id="div2ID">
       <button id="ButtonID" disabled="disabled"> Recognized </button>
       <p id="ScannDataID" style="padding-left: 30px"> Information: {{ this.scannDataNumber }}</p>
     </div>
-
     <div id="ProduktFensterID" v-if="visible==true"> 
       <div > 
-        <img id="ProductPictureID" :src="Datenbank[this.scannDataNumber].src" /> <!-- Produktfoto -->
+        <img id="ProductPictureID" :src=localDataObject.src />  <!-- Funktioniert nicht !!?-->
       </div>
       <div> 
         <ul>
           <li> {{ this.localDataObject.name }} </li> 
-          <li> {{ this.localDataObject.Preis }} </li> 
-          <li> {{ this.localDataObject.Größe }} </li>
+          <li> {{ this.localDataObject.preis + " €"}} </li> 
+          <li> {{ this.localDataObject.größe + " Liter"}} </li>
         </ul>
       </div>
     </div>
@@ -28,46 +26,35 @@ import { StreamBarcodeReader } from "vue-barcode-reader";
 export default {
   data() {
     return {
-      scannDataNumber: "text",
+      scannDataNumber: "ean",
       visible: false,
 
-      //lokales Datenobjekt für Produktausgabe
+      //lokales Datenobjekt für Produktausgabe  ->  bei Start "leer"
       localDataObject: {
         name: null,
-        preis: null,
-        pfand: null,  
+        preis: null, 
         größe: null,    
-        src: null,
+        src: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61MypS1KawL._AC_SS450_.jpg",
       },
 
 //------------------------------------------------------------------------------------------------------------------------------------
 
-       //lokale Datenbank  --> hier wäre eigentlich eine API/Datenbank im einsatz 
-      Datenbank: { 
-        4059549000152: {
-        name: "Atla",
-        preis: 2.99,
-        PfandStatus: false,
-        größe: 72, 
-        src: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61MypS1KawL._AC_SS450_.jpg"
-        },
-        123456789012: {
-          name: "Cola Light", 
-          Preis: 1.19,
-          Pfand: 0.25,
-          PfandStatus: true,
-          Größe: 1, 
-          src: "https://rexroth-liefert.de/media/9b/17/be/1638257642/6527397_SHOP_CCL_1L_PET_FR_GER_D_CON_EAN5449000017895_R1.png"
-        },
-        7085840170697: {
-          name: "Fuze Tee", 
-          preis: 1.29,
-          pfand: 0.25,
-          PfandStatus: true,
-          Größe: 1, 
-          src: "https://www.worldofsweets.de/out/pictures/master/product/1/fuzetea-schwarzer-tee-pfirsich-400ml-no1-4837.jpg"
-        }
-      }
+          //  "lokale" Datenbank  -->  hier wäre eigentlich eine API/Datenbank im einsatz 
+       datenbank: [
+        { "ean": 123456789012,
+	        "name": "Cola Light",
+	        "preis": 1.29,
+          "größe": 1,
+	        "pictureLink": "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61MypS1KawL._AC_SS450_.jpg"
+	      },
+	       
+        { "ean": 7290011018184,
+	        "name": "Fuze Tee",
+	        "preis": 1.19,
+          "größe": 1,
+	        "pictureLink": "https://www.worldofsweets.de/out/pictures/master/product/1/fuzetea-schwarzer-tee-pfirsich-400ml-no1-4837.jpg"
+	      }
+      ],
 
 //------------------------------------------------------------------------------------------------------------------------------------
 
@@ -77,36 +64,24 @@ export default {
     StreamBarcodeReader
   },
    methods: {
-      //On Decode -> sobald ein Code erkannt wurde    (result = Nummer)
+      //  On Decode -> sobald ein Code erkannt wurde.    (result = Nummer)
     onDecode (result) { 
-    this.scannDataNumber = result; 
-
-      if(result == "4059549000152"){ //Kreide 
-        console.log("Scan war erfolgreich: " + result);
-        this.visible=true;
-        document.getElementById("ButtonID").style.background='#008000'; 
-        this.localDataObject.name  = this.Datenbank[4059549000152].name;
-        this.localDataObject.preis = this.Datenbank[4059549000152].preis;
-        this.localDataObject.größe = this.Datenbank[4059549000152].größe;
-      }  
-
-      if(result == "123456789012"){ //Cola Light
-        console.log("Scan war erfolgreich: " + result);
-        this.visible=true;
-        document.getElementById("ButtonID").style.background='#008000'; 
-        this.localDataObject.name  = this.Datenbank[123456789012].name;
-        this.localDataObject.preis = this.Datenbank[123456789012].preis;
-        this.localDataObject.größe = this.Datenbank[123456789012].größe;
+      this.scannDataNumber = result; 
+      
+      for(let i=0; i<2; i++){                                               //   Anzahl-Elemente im Array
+        if(result == this.datenbank[i].ean){                                //   prüft ob EAN in "Datenbank" liegt
+          console.log("match mit:" + this.datenbank[i].ean);
+          document.getElementById("ButtonID").style.background='#008000';   //   Button wird grün bei match
+          this.localDataObject.name  = this.datenbank[i].name;              //   Name  übergeben
+          this.localDataObject.preis = this.datenbank[i].preis;             //   Preis übergeben
+          this.localDataObject.größe = this.datenbank[i].größe;             //   Größe übergeben
+          this.localDataObject.src   = this.datenbank[i].src;               //   SRC   übergeben
+          this.visible=true;
+        } else {
+          document.getElementById("ButtonID").style.background='#f82c00';   // Button wird rot bei keinem match
+          //this.visible=false;
+        } 
       } 
-     
-      if(result == "7085840170697"){ //Fuze Tee
-        console.log("Scan war erfolgreich: " + result);
-        this.visible=true;
-        document.getElementById("ButtonID").style.background='#008000'; 
-        this.localDataObject.name  = this.Datenbank[7085840170697].name;
-        this.localDataObject.preis = this.Datenbank[7085840170697].preis;
-        this.localDataObject.größe = this.Datenbank[7085840170697].größe;
-      }
     }
   } 
 }
