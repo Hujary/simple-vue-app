9c963b56c0c563827551f04a3385b85551e86a0d
Hujary <petzold.tom2000@gmail.com>
2023-03-23 11:32:26 +0100

    a

--- src/App.vue
+++ src/App.vue
@@ -11,10 +11,10 @@
 
     <div id="ProduktFensterID" v-if="visible==true"> 
       <div > 
-        <img id="ProductPictureID" src="https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61MypS1KawL._AC_SS450_.jpg" /> 
+        <img id="ProductPictureID" :src="Datenbank[this.ScannData].src" /> 
       </div>
       <div> 
-        {{ Datenbank[4059549000152].name }}
+        {{ Datenbank[123456789012].name }}
       </div>
     </div>
 </template>
@@ -28,8 +28,17 @@ export default {
       visible: false,
       Datenbank: { 
         4059549000152: {
-          name: "Atla", Qualität: "für alle Tafeln geeignet", Größe: "72 Stück", BildURL: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61MypS1KawL._AC_SS450_.jpg"
-       }
+          name: "Atla",
+          Qualität: "für alle Tafeln geeignet",
+          Größe: "72 Stück", 
+          src: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61MypS1KawL._AC_SS450_.jpg"
+        },
+        123456789012: {
+          name: "Test", 
+          Qualität: "Gut",
+          Größe: "Leer", 
+          src: "https://www.hellabrunn.de/fileadmin/_processed_/c/f/csm_tierpark-hellabrunn-wasserschwein-capybara-amerika-tierlexikon-artenschutz_7c6c6695e6.png"
+        }
       }
     }
   },
@@ -37,13 +46,25 @@ export default {
     StreamBarcodeReader
   },
    methods: {
-     onDecode (result) { 
-      this.ScannData = result;
+    onDecode (result) { 
+    this.ScannData = result; 
 
       if(result == "4059549000152"){
+        console.log("Scan war erfolgreich: " + result);
         this.visible=true;
         document.getElementById("ButtonID").style.background='#008000';
       }  
+
+      if(result == "123456789012"){
+        console.log("Scan war erfolgreich: " + result);
+        this.visible=true;
+        document.getElementById("ButtonID").style.background='#008000';
+      } 
+
+    /*for(let i=0; i<Datenbank.length(); i++){
+      console.log("funktioniert");
+    } */
+    
     }
   } 
 }
