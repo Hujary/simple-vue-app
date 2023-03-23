d9bd5c6c55987037ca7a85f767d48afb9690e90c
Hujary <petzold.tom2000@gmail.com>
2023-03-23 20:36:10 +0100

    bugfixing

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,7 @@
     </div>
     <div id="ProduktFensterID" v-if="visible"> 
       <div > 
-        <img id="ProductPictureID" :src=localDataObject.src />  <!-- Funktioniert nicht !!?-->
+        <img id="ProductPictureID" :src=this.localDataObject.src />  <!-- Funktioniert nicht !!?-->
       </div>
       <div> 
         <ul style="padding-top: 35px">
@@ -26,7 +26,7 @@ import { StreamBarcodeReader } from "vue-barcode-reader";
 export default {
   data() {
     return {
-      scannDataNumber: "ean",
+      scannDataNumber: "barcode",
       visible: false,
 
         //  lokales Datenobjekt für Produktausgabe  ->  bei Start "leer"
@@ -34,27 +34,27 @@ export default {
         name: null,
         preis: null, 
         größe: null,    
-        src: "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61MypS1KawL._AC_SS450_.jpg",
+        src: null,
       },
 
 //------------------------------------------------------------------------------------------------------------------------------------
 
           //  "lokale" Datenbank  -->  hier wäre eigentlich eine API/Datenbank im einsatz 
        datenbank: [
-        { "ean": 123456789012,
+        { "barcode": 123456789012,
 	        "name": "Cola Light",
 	        "preis": 1.29,
           "größe": 1,
 	        "pictureLink": "https://m.media-amazon.com/images/W/IMAGERENDERING_521856-T1/images/I/61MypS1KawL._AC_SS450_.jpg"
 	      },
 	       
-        { "ean": 7290011018184,
+        { "barcode": 7290011018184,
 	        "name": "Fuze Tee",
 	        "preis": 1.19,
           "größe": 1,
 	        "pictureLink": "https://www.worldofsweets.de/out/pictures/master/product/1/fuzetea-schwarzer-tee-pfirsich-400ml-no1-4837.jpg"
 	      },
-        { "ean": 1231231231232,
+        { "barcode": 1231231231232,
 	        "name": "test",
 	        "preis": 1.19,
           "größe": 2,
@@ -73,20 +73,24 @@ export default {
       //  On Decode -> sobald ein Code erkannt wurde.    (result = Nummer)
     onDecode (result) { 
       this.scannDataNumber = result; 
+      this.visible = false;
+      let prüfvariable;
       
       for(let i=0; i<3; i++){                                               //   Anzahl-Elemente im Array
-        if(result == this.datenbank[i].ean){                                //   prüft ob EAN in "Datenbank" liegt
-          console.log("match mit:" + this.datenbank[i].ean);
+        console.log(i);
+        if(this.scannDataNumber == this.datenbank[i].barcode){              //   prüft ob EAN in "Datenbank" liegt
+          console.log("match mit:" + this.datenbank[i].barcode);
           document.getElementById("ButtonID").style.background='#008000';   //   Button wird grün bei match
           this.localDataObject.name  = this.datenbank[i].name;              //   Name  übergeben
           this.localDataObject.preis = this.datenbank[i].preis;             //   Preis übergeben
           this.localDataObject.größe = this.datenbank[i].größe;             //   Größe übergeben
-          this.localDataObject.src   = this.datenbank[i].src;               //   SRC   übergeben
+          this.localDataObject.barcode   = this.datenbank[i].barcode;       //   SRC   übergeben
           this.visible=true;
-        } else {
-          document.getElementById("ButtonID").style.background='#f82c00';   // Button wird rot bei keinem match
-          this.visible=false;
-        } 
+          this.prüfvariable = true;
+        }
+      } 
+      if(prüfvariable == false) {
+        document.getElementById("ButtonID").style.background='#f82c00';   // Button wird rot bei keinem match 
       } 
     }
   } 
